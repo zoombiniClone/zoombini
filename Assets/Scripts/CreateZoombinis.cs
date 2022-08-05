7f74f524d8f55709ca02d9ecbf1760a72ae9787e
ybs1164 <ybs1164@naver.com>
2022-08-05 17:07:32 +0900

    fix bug of layer

--- Assets/Scripts/CreateZoombinis.cs
+++ Assets/Scripts/CreateZoombinis.cs
@@ -6,40 +6,42 @@ public class CreateZoombinis : MonoBehaviour
 {
     [SerializeField] private GameObject zoombiniPrefab;
     [SerializeField] private int count = 16;
+    [SerializeField] private float gap = 1;
 
     void Start()
     {
-        int len = (int)Mathf.Sqrt(count);
+        int len = (int)Mathf.Round(Mathf.Sqrt(count));
         int less = count;
+        Vector3 startPosition = new Vector3(-len * gap * 0.5f, -len * gap * 0.5f, 0);
 
         for (int x = 0; x < len; x++)
         {
             for (int y = 0; y < len; y++)
             {
-                GameObject clone = Instantiate(zoombiniPrefab, new Vector3(x, y, 0), Quaternion.identity);
-                Zoombini zoombini = clone.GetComponent<Zoombini>();
+                if (count <= 0) break;
 
-                zoombini.ChangeType(
-                    Random.Range(0, 5),
-                    Random.Range(0, 5),
-                    Random.Range(0, 5),
-                    Random.Range(0, 5)
-                );
+                CreateZoombini(startPosition + new Vector3(x * gap, y * gap - x * 0.01f, 0));
+
                 count--;
             }
         }
 
-        for (int x = 0; x < count; x++)
+        for (int y = 0; y < count; y++)
         {
-            GameObject clone = Instantiate(zoombiniPrefab, new Vector3(x, len, 0), Quaternion.identity);
-            Zoombini zoombini = clone.GetComponent<Zoombini>();
-
-            zoombini.ChangeType(
-                Random.Range(0, 5),
-                Random.Range(0, 5),
-                Random.Range(0, 5),
-                Random.Range(0, 5)
-            );
+            CreateZoombini(startPosition + new Vector3(len * gap, y * gap - len * 0.01f, 0));
         }
+    }
+
+    void CreateZoombini(Vector3 pos)
+    {
+        GameObject clone = Instantiate(zoombiniPrefab, pos, Quaternion.identity);
+        Zoombini zoombini = clone.GetComponent<Zoombini>();
+
+        zoombini.ChangeType(
+            Random.Range(0, 5),
+            Random.Range(0, 5),
+            Random.Range(0, 5),
+            Random.Range(0, 5)
+        );
     }
 }
