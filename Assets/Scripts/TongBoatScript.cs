8f32f7e8c39d594fdad81599284227a3a7f81be5
SangYoonLee51 <yunisy51@gmail.com>
2022-08-09 23:36:00 +0900

    Stump map

--- Assets/Scripts/TongBoatScript.cs
+++ Assets/Scripts/TongBoatScript.cs
@@ -4,12 +4,33 @@ using UnityEngine;
 
 public class TongBoatScript : MonoBehaviour
 {
-    // 스텀프를 배열화하여 알고리즘 설계할 코드
-    public GameObject[] Stumps;
-
-    void Update()
+    // making map
+    [SerializeField] private GameObject stumpPrefab;
+    [SerializeField] private int count = 16;
+    
+    void Start()
     {
-        
+
+        int len = count;
+        Vector3 startPosition = new Vector3(-len * 0.5f, 0 * 0.5f, 0);
+
+        for (int x = 0; x < len; x++)
+        {
+            CreateTongBoat(startPosition + new Vector3(x * 1.3f, 0, 0));
+        }
+    
+    
+    }
+
+    void CreateTongBoat(Vector3 pos)
+    {
+        GameObject clone = Instantiate(stumpPrefab, pos, Quaternion.identity); // Quaternion.identity
+        clone.transform.Rotate(60f, 0f, 0f);
+        Stump stump = clone.GetComponent<Stump>();
+
+        int num = Random.Range(0,4);
+        stump.ChangeType(num);
+
     }
 
 
