2c0c5288381c2f3f674769b9a2876faa9ab64a40
SangYoonLee51 <yunisy51@gmail.com>
2022-08-12 21:53:02 +0900

    TongBoat algorithm

--- Assets/Scripts/StumpScript.cs
+++ Assets/Scripts/StumpScript.cs
@@ -8,16 +8,24 @@ public class StumpScript : MonoBehaviour
     [SerializeField] private SpriteRenderer spriteRenderer;
     // Save Target
     private List<GameObject> victims = new List<GameObject>();
+    private List<StumpScript> linkedStumps = new List<StumpScript>();
+
+    private GameObject victim = null;
     private bool isFull = false;
 
     public void SetLinkedStumps(List<StumpScript> stumps)
     {
-        return;
+        linkedStumps = stumps;
     }
 
+    public void AddLinkedStump(StumpScript stump)
+    {
+        linkedStumps.Add(stump);
+    }
+    
     public GameObject GetZoombini()
     {
-        return null;
+        return victim;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -45,7 +53,25 @@ public class StumpScript : MonoBehaviour
     public void OnTouchFinish(GameObject obj)
     {
         if (!victims.Contains(obj)) return;
+
+        int[] objTypes = obj.GetComponent<Zoombini>().GetType();
+        foreach (StumpScript stump in linkedStumps)
+        {
+            if (stump.GetZoombini() == null) continue;
+
+            int[] stumpTypes = stump.GetZoombini().GetComponent<Zoombini>().GetType();
+
+            if (stumpTypes[0] != objTypes[0] 
+             && stumpTypes[1] != objTypes[1]
+             && stumpTypes[2] != objTypes[2]
+             && stumpTypes[3] != objTypes[3]) 
+            {
+                return;
+            }
+        }
+
         isFull = true;
+        victim = obj;
         obj.transform.position = transform.position;
     }
 
@@ -53,5 +79,6 @@ public class StumpScript : MonoBehaviour
     {
         if (!victims.Contains(obj)) return;
         isFull = false;
+        victim = null;
     }
 }
